42de4b36632e6594a3e50475aa470db2bcd7b54e
daffadon <daffaputranarendra9@gmail.com>
2025-09-15 13:05:52 +0700

    feat(infra): update CommandRunner interface to include directory parameter in Run method

--- internal/infra/command_runner.go
+++ internal/infra/command_runner.go
@@ -3,13 +3,19 @@ package infra
 import "os/exec"
 
 type CommandRunner interface {
-	Run(name string, args ...string) error
+	Run(name string, args []string, dir string) error
+}
+type runner struct{}
+
+func NewCommandRunner() CommandRunner {
+	return &runner{}
 }
 
-type ShellRunner struct{}
-
-func (ShellRunner) Run(name string, args ...string) error {
+func (r *runner) Run(name string, args []string, dir string) error {
 	cmd := exec.Command(name, args...)
+	if dir != "" {
+		cmd.Dir = dir
+	}
 	cmd.Stdout = nil
 	cmd.Stderr = nil
 	return cmd.Run()
