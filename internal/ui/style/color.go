70b378a8d29d0335ae49a8ebbf853e8768c4252a
daffadon <daffaputranarendra9@gmail.com>
2025-09-16 09:06:40 +0700

    refactor(style): rename XStyle to BlueStyle for consistency in color definitions

--- internal/ui/style/color.go
+++ internal/ui/style/color.go
@@ -3,8 +3,7 @@ package style
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	XStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color("12")).Bold(true)
-	StepLabelStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("12")).Bold(true)
-	ArrowStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("10")).Bold(true)
-	ErrorStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("9")).Bold(true)
+	BlueStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("12")).Bold(true)
+	ArrowStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("10")).Bold(true)
+	ErrorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("9")).Bold(true)
 )
