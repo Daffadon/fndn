a3601e24800433dd65babed2bd85c11b6157e19e
daffadon <daffaputranarendra9@gmail.com>
2025-09-19 16:44:57 +0700

    feat(template): enhance ENV configuration template with detailed comments and structure

--- internal/template/config/env.go
+++ internal/template/config/env.go
@@ -3,6 +3,14 @@ package config_template
 const ENVConfigTemplate string = `
 package env
 
+	import 	"github.com/spf13/viper"
+
+	// config.yaml that loaded by this code in production environment (container)
+	// can be achieved by mount the config file to the container at / (default) or read the config file
+	// from remote source, which can you do based on docs (https://github.com/spf13/viper)
+
+	// or (which i don't suggest but working) inject the config.yaml in the Dockerfile.
+
 func Load() {
 	env := os.Getenv("ENV")
 	configName := "config.local"
@@ -21,4 +29,28 @@ func Load() {
 		panic("failed to read config")
 	}
 }
+`
+
+const DotENVExampleTemplate string = `
+# copy this file and change name to .env and fill the variable
+# to be used by docker compose
+
+# app env
+ENV=
+
+# db env
+DB_USER=
+DB_PASSWORD=
+DB_NAME=
+
+# nats env
+NATS_USER=
+NATS_PASSWORD=
+
+# redis env
+REDIS_PASSWORD=
+
+# minio env
+MINIO_ROOT_USER=
+MINIO_ROOT_PASSWORD=
 `
