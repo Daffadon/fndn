551fe26ab44a5b7ff4517d60cb734e09e42711ce
daffadon <daffaputranarendra9@gmail.com>
2025-09-19 16:45:16 +0700

    feat(template): add Docker Compose configuration for Redis cache

--- internal/template/cache/redis.go
+++ internal/template/cache/redis.go
@@ -5,6 +5,8 @@ package cache
 
 import (
 	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog"
+	"github.com/spf13/viper"
 )
 
 func NewRedisConnection(zerolog zerolog.Logger) (*redis.Client, error) {
@@ -23,3 +25,21 @@ func NewRedisConnection(zerolog zerolog.Logger) (*redis.Client, error) {
 	return client, nil
 }
 `
+
+const DockerComposeRedisConfigTemplate string = `
+# redis
+  {{.ProjectName}}_cache:
+    image: redis:7.2
+    container_name: {{.ProjectName}}_cache
+    volumes:
+      - {{.ProjectName}}_redis_data:/data
+    restart: unless-stopped
+		ports:
+      - "6379:6379"
+    environment:
+      - REDIS_PASSWORD=${REDIS_PASSWORD}
+    command: ["redis-server", "--requirepass", "$REDIS_PASSWORD"]
+`
+
+const DockerComposeRedisVolumeTemplate string = `
+  {{.ProjectName}}_redis_data: {}`
