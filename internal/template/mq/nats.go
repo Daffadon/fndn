1941c390543995be9bbd11b10ce459e18a4cb175
daffadon <daffaputranarendra9@gmail.com>
2025-09-19 16:44:26 +0700

    feat(template): add NATS configuration templates and connection setup

--- internal/template/mq/nats.go
+++ internal/template/mq/nats.go
@@ -3,6 +3,11 @@ package mq_template
 const NatsConfigTemplate string = `
 package mq
 
+import(
+	"github.com/spf13/viper"
+	"github.com/nats-io/nats.go"
+)
+
 func NewNatsConnection() *nats.Conn {
 	addr := fmt.Sprintf("%s://%s:%s", viper.GetString("nats.protocol"), viper.GetString("nats.address"), viper.GetString("nats.port"))
 	nc, err := nats.Connect(addr,
@@ -17,5 +22,47 @@ func NewNatsConnection() *nats.Conn {
 		panic("failed to connect to nats server")
 	}
 	return nc
+}
+`
+
+const DockerComposeNatsConfigTemplate string = `
+# nats
+  {{.ProjectName}}_mq:
+    image: nats:2.11.9
+    container_name: {{.ProjectName}}_mq
+    restart: unless-stopped
+    volumes:
+      - ./config/mq/nats-server.conf:/etc/nats/nats.conf
+      - {{.ProjectName}}_nats_data:/nats:rw
+      - {{.ProjectName}}_nats_jetstream-data:/jetstream/data:rw
+    environment:
+      - NATS_USER=${NATS_USER}
+      - NATS_PASSWORD=${NATS_PASSWORD}
+    ports:
+      -	"8081:8081"
+    command: "-c /etc/nats/nats.conf --name nats -p 4221"
+`
+
+const DockerComposeNatsVolumeTemplate string = `
+  {{.ProjectName}}_nats_data: {}
+  {{.ProjectName}}_nats_jetstream-data: {}`
+
+const NatsConfigFileTemplate string = `
+port 4221
+
+authorization {
+  user: $NATS_USER
+  password: $NATS_PASSWORD
+}
+
+jetstream {
+  store_dir: /jetstream/data
+  max_mem: 1GiB
+  max_file: 100GiB
+}
+
+websocket {
+  port: 8081
+  no_tls: true
 }
 `
