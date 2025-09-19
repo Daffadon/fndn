9b1ebdba278f8f2a98b79abc85703242b37425c6
daffadon <daffaputranarendra9@gmail.com>
2025-09-19 16:44:18 +0700

    feat(template): add Docker Compose configuration for Minio storage

--- internal/template/object_storage/minio.go
+++ internal/template/object_storage/minio.go
@@ -6,6 +6,7 @@ package storage
 import 	(
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/spf13/viper"
 )
 
 func NewMinioConnection() *minio.Client {
@@ -24,3 +25,28 @@ func NewMinioConnection() *minio.Client {
 	return minioClient
 }
 `
+
+const DockerComposeMinioConfigTemplate string = `
+# minio
+  {{.ProjectName}}_storage:
+    image: minio/minio:RELEASE.2025-05-24T17-08-30Z-cpuv1
+    container_name: {{.ProjectName}}_storage
+    environment:
+      MINIO_ROOT_USER: ${MINIO_ROOT_USER}
+      MINIO_ROOT_PASSWORD: ${MINIO_ROOT_PASSWORD}
+    restart: unless-stopped
+    ports:
+      - "9000:80"
+      - "9001:9001"
+    volumes:
+      - {{.ProjectName}}_minio_data:/data
+    command: server /data  --address ":80" --console-address ":9001"
+    healthcheck:
+      test: ["CMD", "curl", "-f", "http://localhost:80/minio/health/live"]
+      interval: 30s
+      timeout: 10s
+      retries: 5
+`
+
+const DockerComposeMinioVolumeTemplate string = `
+  {{.ProjectName}}_minio_data: {}`
