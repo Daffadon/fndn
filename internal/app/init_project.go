73b173563d4432efc817b76442492451204328c4
daffadon <daffaputranarendra9@gmail.com>
2025-09-22 09:19:16 +0700

    feat(init): replace command runner for directory creation with os.MkdirAll and add additional initialization functions

--- internal/app/init_project.go
+++ internal/app/init_project.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"os"
 	"sync"
 
 	"github.com/daffadon/fndn/internal/domain"
@@ -14,11 +15,12 @@ type InitProjectUseCase struct {
 func (uc *InitProjectUseCase) Run(p *domain.Project) error {
 	if p.Path == "" {
 		newPath := p.Name
-		if err := uc.Runner.Run("mkdir", []string{newPath}, ""); err != nil {
+		if err := os.MkdirAll(newPath, 0755); err != nil {
 			return err
 		}
 		p.Path = newPath
 	}
+	// init
 	// create project and init
 	if err := domain.InitProject(uc.Runner, p.Path, p.ModuleName); err != nil {
 		return err
@@ -29,6 +31,7 @@ func (uc *InitProjectUseCase) Run(p *domain.Project) error {
 	}
 	// run each init in a goroutine
 	initFuncs := []func() error{
+		// config
 		func() error { return domain.InitGin(uc.Runner, &p.Path) },
 		func() error { return domain.InitENVConfig(uc.Runner, &p.Path) },
 		func() error { return domain.InitZerologConfig(uc.Runner, &p.Path) },
@@ -36,11 +39,35 @@ func (uc *InitProjectUseCase) Run(p *domain.Project) error {
 		func() error { return domain.InitNatsConfig(uc.Runner, &p.Path) },
 		func() error { return domain.InitMinioConfig(uc.Runner, &p.Path) },
 		func() error { return domain.InitPostgresqlConfig(uc.Runner, &p.Path) },
+
+		// infra
+		func() error { return domain.InitQuerierInfra(uc.Runner, &p.Path) },
+		func() error { return domain.InitRedisInfra(uc.Runner, &p.Path) },
+		func() error { return domain.InitJetstreamInfra(uc.Runner, &p.Path) },
+		func() error { return domain.InitMinioInfra(uc.Runner, &p.Path) },
+
+		// domain
+		func() error { return domain.InitDTODomain(uc.Runner, &p.Path) },
+		func() error { return domain.InitRepositoryDomain(uc.Runner, &p.Path, p.ModuleName) },
+		func() error { return domain.InitServiceDomain(uc.Runner, &p.Path) },
+		func() error { return domain.InitHandlerDomain(uc.Runner, &p.Path) },
+		func() error { return domain.InitHTTPHandlerDomain(uc.Runner, &p.Path) },
+
+		// cmd
 		func() error { return domain.InitDependencyInjection(uc.Runner, &p.Path) },
 		func() error { return domain.InitBootStrap(uc.Runner, &p.Path) },
 		func() error { return domain.InitServer(uc.Runner, &p.Path) },
 		func() error { return domain.InitMain(uc.Runner, &p.Path) },
+
+		// global config
 		func() error { return domain.InitAirConfig(uc.Runner, &p.Path, p.Air) },
+		func() error { return domain.InitYamlConfig(uc.Runner, p, &p.Path) },
+		func() error { return domain.InitGitignoreConfig(uc.Runner, &p.Path) },
+		func() error { return domain.InitDockerFileConfig(uc.Runner, &p.Path, p.Name) },
+		func() error { return domain.InitDockerComposeConfig(uc.Runner, &p.Path, p.Name) },
+		func() error { return domain.InitNatsConfigFile(uc.Runner, &p.Path) },
+		func() error { return domain.InitDotEnvExampleConfig(uc.Runner, &p.Path) },
+		func() error { return domain.InitReadme(uc.Runner, &p.Path) },
 	}
 
 	errCh := make(chan error, len(initFuncs))
@@ -65,6 +92,9 @@ func (uc *InitProjectUseCase) Run(p *domain.Project) error {
 		}
 	}
 	if err := uc.Runner.Run("goimports", []string{"-w", "."}, p.Path); err != nil {
+		return err
+	}
+	if err := uc.Runner.Run("go", []string{"get", "-u", "./..."}, p.Path); err != nil {
 		return err
 	}
 	if err := uc.Runner.Run("go", []string{"mod", "tidy"}, p.Path); err != nil {
