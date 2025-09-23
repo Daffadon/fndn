56c2c6cfd16c6f07b63c415d2887ea359fbf54f0
daffadon <daffaputranarendra9@gmail.com>
2025-09-23 15:39:18 +0700

    feat(helper): add PadOrTruncate function for string padding and truncation

---
+++ internal/ui/helper/pad_truncate.go
@@ -0,0 +1,17 @@
+package helper
+
+import "fmt"
+
+func PadOrTruncate(s string, width int) string {
+	if width <= 0 {
+		return s
+	}
+	runes := []rune(s)
+	if len(runes) > width {
+		if width <= 1 {
+			return string(runes[:width])
+		}
+		return string(runes[:width-1]) + "…"
+	}
+	return fmt.Sprintf("%-*s", width, s)
+}
