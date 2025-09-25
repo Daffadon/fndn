24b5d5ded937e81ed472f11f8c46c98fe4b282ea
daffadon <daffaputranarendra9@gmail.com>
2025-09-25 10:36:32 +0700

    refactor(template): replace hardcoded framework import with template variables

--- internal/template/main/server.go
+++ internal/template/main/server.go
@@ -4,7 +4,7 @@ const HTTPServerTemplate string = `
 package server
 
 import (
-	"github.com/gin-gonic/gin"
+	{{.FrameworkImport}}
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/nats-io/nats.go"
 	"github.com/redis/go-redis/v9"
@@ -22,7 +22,7 @@ func (s *Server) Run(ctx context.Context) {
 	err := s.Container.Invoke(
 		func(
 			logger zerolog.Logger,
-			router *gin.Engine,
+			router {{.FrameworkRouter}},
 			redis *redis.Client,
 			nc *nats.Conn,
 			pgx *pgxpool.Pool,
@@ -42,12 +42,9 @@ func (s *Server) Run(ctx context.Context) {
 			}()
 			defer pgx.Close()
 			
-			router.Use(gin.Recovery())
-			
 			// you can register your routes here
 			// for the example and implementation, here is the example
 
-			
 			handler.RegisterTodoRoutes(router,th)
 			
 			srv := &http.Server{
