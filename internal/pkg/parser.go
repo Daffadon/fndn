6eb40427e67f18f1c1e4f1938d8dcfb757b31cd5
daffadon <daffaputranarendra9@gmail.com>
2025-09-25 17:11:17 +0700

    fix(parser): standardize router handler variable to 'r' for consistency

--- internal/pkg/parser.go
+++ internal/pkg/parser.go
@@ -26,21 +26,25 @@ func HTTPServerParser(fwk string) (string, error) {
 	case "gin":
 		t.FrameworkImport = `"github.com/gin-gonic/gin"`
 		t.FrameworkRouter = "*gin.Engine"
-		t.RouterHandler = "router"
+		t.RouterHandler = "r"
 	case "chi":
 		t.FrameworkImport = `"github.com/go-chi/chi/v5"`
 		t.FrameworkRouter = "*chi.Mux"
-		t.RouterHandler = "router"
+		t.RouterHandler = "r"
 	case "echo":
 		t.FrameworkImport = `"github.com/labstack/echo/v4"`
 		t.FrameworkRouter = "*echo.Echo"
-		t.RouterHandler = "router"
+		t.RouterHandler = "r"
 	case "fiber":
 		t.FrameworkImport = `"github.com/gofiber/fiber/v2"
 		"github.com/gofiber/fiber/v2/middleware/adaptor"
 		`
 		t.FrameworkRouter = "*fiber.App"
-		t.RouterHandler = "adaptor.FiberApp(router)"
+		t.RouterHandler = "adaptor.FiberApp(r)"
+	case "gorrila/mux":
+		t.FrameworkImport = `"github.com/gorilla/mux"`
+		t.FrameworkRouter = "*mux.Router"
+		t.RouterHandler = "router.WarpWithCors(r)"
 	}
 	return ParseTemplate(main_template.HTTPServerTemplate, t)
 }
