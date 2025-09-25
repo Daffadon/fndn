9584306a2ace93abf872f2efa05700d7d0d6ab84
daffadon <daffaputranarendra9@gmail.com>
2025-09-25 10:43:57 +0700

    feat(parser): add HTTPServerParser function and HTTPServerParse type for framework handling

--- internal/pkg/parser.go
+++ internal/pkg/parser.go
@@ -3,6 +3,9 @@ package pkg
 import (
 	"bytes"
 	"text/template"
+
+	main_template "github.com/daffadon/fndn/internal/template/main"
+	"github.com/daffadon/fndn/internal/types"
 )
 
 func ParseTemplate(tmplStr string, data interface{}) (string, error) {
@@ -15,4 +18,20 @@ func ParseTemplate(tmplStr string, data interface{}) (string, error) {
 		return "", err
 	}
 	return buf.String(), nil
+}
+
+func HTTPServerParser(fwk string) (string, error) {
+	var t types.HTTPServerParse
+	switch fwk {
+	case "gin":
+		t.FrameworkImport = `"github.com/gin-gonic/gin"`
+		t.FrameworkRouter = "*gin.Engine"
+	case "chi":
+		t.FrameworkImport = `"github.com/go-chi/chi/v5"`
+		t.FrameworkRouter = "*chi.Mux"
+	case "echo":
+		t.FrameworkImport = `"github.com/labstack/echo/v4"`
+		t.FrameworkRouter = "*echo.Echo"
+	}
+	return ParseTemplate(main_template.HTTPServerTemplate, t)
 }
---
+++ internal/types/http_server.go
@@ -0,0 +1,8 @@
+package types
+
+type (
+	HTTPServerParse struct {
+		FrameworkImport string
+		FrameworkRouter string
+	}
+)
