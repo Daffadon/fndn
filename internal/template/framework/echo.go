b2a7e5ef449d33c96bc377743a467bf639d987e6
daffadon <daffaputranarendra9@gmail.com>
2025-09-25 10:45:59 +0700

    feat(template): add EchoConfigTemplate for Echo framework router setup

---
+++ internal/template/framework/echo.go
@@ -0,0 +1,39 @@
+package framework_template
+
+const EchoConfigTemplate string = `
+package router 
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func NewHTTP() *echo.Echo {
+	env := os.Getenv("ENV")
+	e := echo.New()
+
+	if env == "production" {
+			e.Debug = false
+			e.HideBanner = true
+			e.HidePort = true
+	}
+
+	allowOrigins := viper.GetString("server.cors.allow_origins")
+	allowMethods := viper.GetString("server.cors.allow_methods")
+	allowHeaders := viper.GetString("server.cors.allow_headers")
+	exposeHeaders := viper.GetString("server.cors.expose_headers")
+	allowCredentials := viper.GetBool("server.cors.allow_credential")
+	maxAge := viper.GetInt("server.cors.max_age")
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowOrigins:     strings.Split(allowOrigins, ","),
+			AllowMethods:     strings.Split(allowMethods, ","),
+			AllowHeaders:     strings.Split(allowHeaders, ","),
+			ExposeHeaders:    strings.Split(exposeHeaders, ","),
+			AllowCredentials: allowCredentials,
+			MaxAge:           int(time.Duration(maxAge).Seconds()),
+	}))
+
+	return e
+}
+`
