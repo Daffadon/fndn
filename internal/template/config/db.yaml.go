35a902fe2f620cd941c9b79846d2197b6efd2d17
daffadon <daffaputranarendra9@gmail.com>
2025-09-29 11:31:35 +0700

    feat(template): restructure database YAML config templates to include SQL protocol

--- internal/template/config/db.yaml.go
+++ internal/template/config/db.yaml.go
@@ -2,10 +2,22 @@ package config_template
 
 const PostresqlYamlConfigTemplate string = `
 database:
-  host: "localhost"
-  user: "myusername"
-  password: "password"
-  port: "5432"
-  name: "database_name"
-  sslmode: "disable"
+  sql:
+    protocol: "postgresql"
+    host: "localhost"
+    user: "myusername"
+    password: "password"
+    port: "5432"
+    name: "database_name"
+    sslmode: "disable"
+`
+
+const MariaDBYamlConfigTemplate string = `
+database:
+  sql:
+    host: "localhost"
+    user: "myusername"
+    password: "password"
+    port: "3306"
+    name: "database_name"
 `
