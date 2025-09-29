9c57fd53300ba35d0c4640978f4f89de1fce0a09
daffadon <daffaputranarendra9@gmail.com>
2025-09-29 11:31:00 +0700

    feat(template): update QuerierPgxInfraTemplate and add QuerierMariaDBInfraTemplate

--- internal/template/infra/querier.go
+++ internal/template/infra/querier.go
@@ -1,6 +1,6 @@
 package infra_template
 
-const QuerierInfraTemplate = `
+const QuerierPgxInfraTemplate = `
 package storage_infra
 
 import 	(
@@ -34,5 +34,39 @@ func (p *pgxQuerier) Exec(ctx context.Context, sql string, args ...any) (pgconn.
 
 func (p *pgxQuerier) Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error) {
 	return p.pgx.Query(ctx, sql, args...)
+}
+`
+
+const QuerierMariaDBInfraTemplate = `
+package storage_infra
+
+import "database/sql"
+
+type (
+	Querier interface {
+			QueryRow(ctx context.Context, query string, args ...any) *sql.Row
+			Exec(ctx context.Context, query string, args ...any) (sql.Result, error)
+			Query(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+	}
+
+	sqlQuerier struct {
+			db *sql.DB
+	}
+)
+
+func NewQuerier(db *sql.DB) Querier {
+	return &sqlQuerier{db: db}
+}
+
+func (q *sqlQuerier) QueryRow(ctx context.Context, query string, args ...any) *sql.Row {
+	return q.db.QueryRowContext(ctx, query, args...)
+}
+
+func (q *sqlQuerier) Exec(ctx context.Context, query string, args ...any) (sql.Result, error) {
+	return q.db.ExecContext(ctx, query, args...)
+}
+
+func (q *sqlQuerier) Query(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
+	return q.db.QueryContext(ctx, query, args...)
 }
 `
