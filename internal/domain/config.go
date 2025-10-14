b6cafe1e57bc92f7c7af51fab82d85350126b7ce
daffadon <daffaputranarendra9@gmail.com>
2025-10-14 14:56:30 +0700

    feat(config): implement RabbitMQ support and refactor message queue configuration

--- internal/domain/config.go
+++ internal/domain/config.go
@@ -44,8 +44,14 @@ func InitYamlConfig(i infra.CommandRunner, p *Project) error {
 		}
 		s += config_template.AppYamlConfigTemplate
 		s += config_template.RedisYamlConfigTemplate
-		s += config_template.NatsYamlConfigTemplate
-		s += config_template.JetstreamConfigTemplate
+
+		switch p.MQ {
+		case "nats":
+			s += config_template.NatsYamlConfigTemplate
+			s += config_template.JetstreamConfigTemplate
+		case "rabbitmq":
+			s += config_template.RabbitYamlConfigTemplate
+		}
 		s += config_template.MinioYamlConfigTemplate
 		s += config_template.ServerYamlConfigTemplate
 
