631e3c2ed87eaf23ed5a913fc4bc4ad1c836da98
daffadon <daffaputranarendra9@gmail.com>
2025-11-06 09:15:20 +0800

    feat(cache): refactor in-memory configuration initialization for various cache types

--- internal/domain/cache.go
+++ internal/domain/cache.go
@@ -9,13 +9,47 @@ import (
 	cache_template "github.com/daffadon/fndn/internal/template/cache"
 )
 
-func InitRedisConfig(i infra.CommandRunner, path *string) error {
+func InitInMemoryConfig(i infra.CommandRunner, path *string, inMemory *string) error {
 	if path != nil {
 		folderName := "/config/cache"
-		fileName := folderName + "/redis.go"
-		if err := pkg.GoFileGenerator(i, path, folderName, fileName, cache_template.RedisConfigTemplate); err != nil {
+		var template, fileName string
+		switch *inMemory {
+		case "redis":
+			fileName = folderName + "/redis.go"
+			template = cache_template.RedisConfigTemplate
+		case "valkey":
+			fileName = folderName + "/valkey.go"
+			template = cache_template.ValkeyConfigTemplate
+		case "keydb":
+			fileName = folderName + "/keydb.go"
+			// template = cache_template.RedisConfigTemplate
+		case "dragonfly":
+			fileName = folderName + "/dragonfly.go"
+			// template = cache_template.RedisConfigTemplate
+		}
+		if err := pkg.GoFileGenerator(i, path, folderName, fileName, template); err != nil {
 			log.Fatal(err)
 			return err
+		}
+		return nil
+	}
+	return errors.New("path is nil")
+}
+
+func InitInMemoryConfigFile(i infra.CommandRunner, p *Project) error {
+	if p.Path != nil {
+		folderName := "/config/cache"
+		var fileName, template string
+		switch p.InMemory {
+		case "valkey":
+			fileName = folderName + "/valkey.acl"
+			template = cache_template.ValkeyConfigFileTemplate
+		}
+		if fileName != "" || template != "" {
+			if err := pkg.GenericFileGenerator(i, p.Path, folderName, fileName, template); err != nil {
+				log.Fatal(err)
+				return err
+			}
 		}
 		return nil
 	}
