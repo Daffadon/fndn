292f5d1e62dc4898b3cfd2b70c2b7c9037baa58f
daffadon <daffaputranarendra9@gmail.com>
2025-11-06 09:16:47 +0800

    feat(config): add Valkey configuration template to cache.yaml.go

--- internal/template/config/cache.yaml.go
+++ internal/template/config/cache.yaml.go
@@ -7,3 +7,11 @@ redis:
   client_name: "your_service/app_name"
   password: "password"
 `
+
+const ValkeyYamlConfigTemplate string = `
+valkey:
+  address: "localhost"
+  port: "6379"
+  username: "username"
+  password: "password"
+`
