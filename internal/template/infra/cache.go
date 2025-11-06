711e5d422b8edc557f1af3b9ad2246826336294f
daffadon <daffaputranarendra9@gmail.com>
2025-11-06 12:04:54 +0800

    feat(infra): add DragonFly cache implementation and interface

--- internal/template/infra/cache.go
+++ internal/template/infra/cache.go
@@ -125,3 +125,61 @@ func (v *valkeInfra) Delete(ctx context.Context, key string) error {
 	return nil
 }
 `
+const DragonFlyInfraTemplate string =`
+package cache_infra
+
+import (
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog"
+)
+
+type (
+	DragonflyInfra interface {
+		Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error
+		Get(ctx context.Context, key string) (string, error)
+		Delete(ctx context.Context, key string) error
+	}
+	dragonflyInfra struct {
+		dragonflyClient *redis.Client
+		logger      zerolog.Logger
+	}
+)
+
+func NewDragonflyCache(dragonflyClient *redis.Client, logger zerolog.Logger) DragonflyInfra {
+	return &dragonflyInfra{
+		dragonflyClient: dragonflyClient,
+		logger:      logger,
+	}
+}
+
+func (r *dragonflyInfra) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	err := r.dragonflyClient.Set(ctx, key, value, expiration).Err()
+	if err != nil {
+		r.logger.Error().Err(err).Str("key", key).Msg("failed to set value in dragonfly")
+		return err
+	}
+	return nil
+}
+
+func (r *dragonflyInfra) Get(ctx context.Context, key string) (string, error) {
+	val, err := r.dragonflyClient.Get(ctx, key).Result()
+	if err != nil {
+		if err == redis.Nil {
+			r.logger.Warn().Str("key", key).Msg("key not found in dragonfly")
+			return "", err
+		}
+		r.logger.Error().Err(err).Str("key", key).Msg("failed to get value from dragonfly")
+		return "", err
+	}
+	return val, nil
+}
+
+func (r *dragonflyInfra) Delete(ctx context.Context, key string) error {
+	err := r.dragonflyClient.Del(ctx, key).Err()
+	if err != nil {
+		r.logger.Error().Err(err).Str("key", key).Msg("failed to delete key from dragonfly")
+		return err
+	}
+	return nil
+}
+`
