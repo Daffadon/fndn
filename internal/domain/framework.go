3f68cfe394dfa7c9bae7eba8979240cecd9b6c76
daffadon <daffaputranarendra9@gmail.com>
2026-01-09 14:45:56 +0800

    feat(generation): add GenerateSpecificProject function for framework-specific config generation

--- internal/domain/framework.go
+++ internal/domain/framework.go
@@ -34,3 +34,37 @@ func InitFramework(i infra.CommandRunner, path *string, framework *string) error
 	}
 	return errors.New("path is nil")
 }
+
+func GenerateSpecificProject(framework string, infraRunner infra.CommandRunner, path string) error {
+	// check folder config/router/ exist or not
+	// check filename
+	folderName := "/config/router"
+	fileName := folderName + "/http.go"
+
+	// if exist, the file name add _framework_name
+	exist := pkg.IsFileExists("." + fileName)
+	if exist {
+		fileName = folderName + "/http_" + framework + ".go"
+	}
+
+	var t string
+	switch framework {
+	case "gin":
+		t = framework_template.GinConfigTemplate
+	case "chi":
+		t = framework_template.ChiConfigTemplate
+	case "echo":
+		t = framework_template.EchoConfigTemplate
+	case "fiber":
+		t = framework_template.FiberConfigTemplate
+	case "gorilla/mux":
+		t = framework_template.GorillaMuxConfigTemplate
+	}
+
+	if err := pkg.GoFileGenerator(infraRunner, &path, folderName, fileName, t); err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
+}
