19eb8f9d152065ca758979dd84d9516a47d33d3f
daffadon <daffaputranarendra9@gmail.com>
2026-01-09 16:01:26 +0800

    feat(cli): add database config generation

--- internal/domain/database.go
+++ internal/domain/database.go
@@ -12,27 +12,24 @@ import (
 func InitDBConfig(i infra.CommandRunner, path *string, db *string) error {
 	if path != nil {
 		folderName := "/config/storage"
-		var fileName, template string
+		fileName := folderName + "/db.go"
+		var template string
+
 		switch *db {
 		case "postgresql":
-			fileName = folderName + "/postgresql.go"
 			template = database_template.PostgresqlConfigTemplate
 		case "mariadb":
-			fileName = folderName + "/mariadb.go"
 			template = database_template.MariaDBConfigTemplate
 		case "clickhouse":
-			fileName = folderName + "/clickhouse.go"
 			template = database_template.ClickHouseConfigTemplate
 		case "mongodb":
-			fileName = folderName + "/mongodb.go"
 			template = database_template.MongoDBConfigTemplate
 		case "ferretdb":
-			fileName = folderName + "/ferretdb.go"
 			template = database_template.FerretDBConfigTemplate
 		case "neo4j":
-			fileName = folderName + "/neo4j.go"
 			template = database_template.Neo4jConfigTemplate
 		}
+
 		if err := pkg.GoFileGenerator(i, path, folderName, fileName, template); err != nil {
 			log.Fatal(err)
 			return err
@@ -40,4 +37,40 @@ func InitDBConfig(i infra.CommandRunner, path *string, db *string) error {
 		return nil
 	}
 	return errors.New("path is nil")
+}
+
+func GenerateSpecificDatabase(db string, infraRunner infra.CommandRunner, path string) error {
+	// check folder config/router/ exist or not
+	// check filename
+	folderName := "/config/storage"
+	fileName := folderName + "/db.go"
+
+	// if exist, the file name add _framework_name
+	exist := pkg.IsFileExists("." + fileName)
+	if exist {
+		fileName = folderName + "/db_" + db + ".go"
+	}
+
+	var template string
+	switch db {
+	case "postgresql":
+		template = database_template.PostgresqlConfigTemplate
+	case "mariadb":
+		template = database_template.MariaDBConfigTemplate
+	case "clickhouse":
+		template = database_template.ClickHouseConfigTemplate
+	case "mongodb":
+		template = database_template.MongoDBConfigTemplate
+	case "ferretdb":
+		template = database_template.FerretDBConfigTemplate
+	case "neo4j":
+		template = database_template.Neo4jConfigTemplate
+	}
+
+	if err := pkg.GoFileGenerator(infraRunner, &path, folderName, fileName, template); err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
 }
